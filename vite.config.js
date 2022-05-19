ac1888ce7a6c93e2f33355a07fd5e17cf36dfe12
imzerokim <lucillenayoung@gmail.com>
2022-05-19 13:25:52 +0900

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,24 @@
 // vite.config.js
-const { resolve } = require('path')
-const { defineConfig } = require('vite')
+const { resolve, join } = require('path');
+const { defineConfig } = require('vite');
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 module.exports = defineConfig({
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-      }
-    }
-  }
-})
+      },
+    },
+  },
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'data',
+          dest: join(__dirname, 'dist'),
+        },
+      ],
+    }),
+  ],
+});
